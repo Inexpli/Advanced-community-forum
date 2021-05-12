648dbb5d236162c505f66df25019c41537a6f717
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-05-12 22:46:31 +0200

    Profile, Profile default image, Profile custom image

--- main/main/urls.py
+++ main/main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -26,5 +28,8 @@ urlpatterns = [
     path('', user_views.index, name='index'),
     path('about/', user_views.about, name='about'),
     path('home/', user_views.home, name='home'),
-    path('profile/', user_views.profile, name='profile')
-]
+    path('profile/', user_views.profile, name='profile'),
+]   
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
